d7291ec50959a2ddec07028cfa5776f1f27aba5e
Matteo Vaccari <vaccari@pobox.com>
2017-03-27 15:59:58 +0200

    renamed Main class so that its more easily identified in run configurations

--- src/main/java/com/thoughtworks/videorental/main/VideoWorldMain.java
+++ src/main/java/com/thoughtworks/videorental/main/VideoWorldMain.java
@@ -3,7 +3,7 @@ package com.thoughtworks.videorental.main;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.webapp.WebAppContext;
 
-public final class Main {
+public final class VideoWorldMain {
     public static void main(final String[] args) throws Exception {
         Server server = new Server(8080);
         final WebAppContext webAppContext = new WebAppContext("src/main/webapp", "/");
