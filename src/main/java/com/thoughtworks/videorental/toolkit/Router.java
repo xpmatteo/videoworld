78200123cc1fe385ee272be2c30f3e370e78133e
Matteo Vaccari <vaccari@pobox.com>
2017-03-29 09:22:31 +0200

    simplify router

--- src/main/java/com/thoughtworks/videorental/toolkit/Router.java
+++ src/main/java/com/thoughtworks/videorental/toolkit/Router.java
@@ -1,7 +1,7 @@
 package com.thoughtworks.videorental.toolkit;
 
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.thoughtworks.videorental.main.WebAction;
 
@@ -9,7 +9,7 @@ public class Router {
 	private static final String LOGIN_REDIRECT = "/login";
 	private WebRequest request;
 	private WebResponse response;
-	private SortedMap<String, PossiblyProtectedWebAction> actions = new TreeMap<>();
+	private List<Route> routes = new ArrayList<>();
 
 	public Router(WebRequest request, WebResponse response) {
 		this.request = request;
@@ -17,42 +17,42 @@ public class Router {
 	}
 
 	public void service() {
-		if (!actions.containsKey(request.getPath())) {
-			response.setStatus(404);
-			response.renderText("<h1>Not Found</h1>");
-			return;
+		for (Route route : routes) {
+			if (request.getPath().equals(route.path)) {
+				if (null != request.getCustomer() || !route.isProtected) {
+					route.action.accept(request, response);
+					return;
+				}
+				response.redirectTo(LOGIN_REDIRECT);
+				return;
+			}
 		}
-		if (null == request.getCustomer() && !actions.get(request.getPath()).isProtected) {
-			actions.get(request.getPath()).action.accept(request, response);
-			return;
-		}
-		if (null == request.getCustomer()) {
-			response.redirectTo(LOGIN_REDIRECT);
-			return;
-		}
-		actions.get(request.getPath()).action.accept(request, response);
+		response.setStatus(404);
+		response.renderText("<h1>Not Found</h1>");
 	}
 
 	public void addResource(String path, WebAction action) {
-		actions.put(path, protectedAction(action));
+		routes.add(protectedRoute(path, action));
 	}
 
 	public void addUnprotectedResource(String path, WebAction unprotectedAction) {
-		this.actions.put(path, unProtectedAction(unprotectedAction));
+		routes.add(unProtectedRoute(path, unprotectedAction));
 	}
 
-	private PossiblyProtectedWebAction protectedAction(WebAction action) {
-		return new PossiblyProtectedWebAction(action, true);
+	private Route protectedRoute(String path, WebAction action) {
+		return new Route(path, action, true);
 	}
 
-	private PossiblyProtectedWebAction unProtectedAction(WebAction action) {
-		return new PossiblyProtectedWebAction(action, false);
+	private Route unProtectedRoute(String path, WebAction action) {
+		return new Route(path, action, false);
 	}
 
-	private static class PossiblyProtectedWebAction {
+	private static class Route {
+		String path;
 		WebAction action;
 		Boolean isProtected;
-		public PossiblyProtectedWebAction(WebAction action, Boolean isProtected) {
+		public Route(String path, WebAction action, Boolean isProtected) {
+			this.path = path;
 			this.action = action;
 			this.isProtected = isProtected;
 		}
