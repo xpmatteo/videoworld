0249d1acbacf72111459e9aa67100045ab840f10
Matteo Vaccari <mvaccari@thoughtworks.com>
2017-04-01 09:05:47 +0200

    better tests for ViewCurrentRentalsAction

--- src/unit/java/com/thoughtworks/videorental/action/ViewCurrentRentalsActionTest.java
+++ src/unit/java/com/thoughtworks/videorental/action/ViewCurrentRentalsActionTest.java
@@ -1,9 +1,12 @@
 package com.thoughtworks.videorental.action;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import com.thoughtworks.datetime.FiniteLocalDate;
 import com.thoughtworks.datetime.LocalDate;
 import com.thoughtworks.datetime.LocalDateTime;
 import com.thoughtworks.datetime.Period;
@@ -16,17 +19,36 @@ import com.thoughtworks.videorental.repository.SetBasedTransactionRepository;
 
 public class ViewCurrentRentalsActionTest extends BaseTestForVideoWorldApp {
 
-	private static final Customer CUSTOMER = anyCustomer();
+	private static final Customer CUSTOMER = new Customer("Pippo");
+	private static final Customer ANOTHER_CUSTOMER = new Customer("Topolino");
 	private static final Rental CURRENT_RENTAL_1 = aRentalExpiring(tomorrow());
 	private static final Rental CURRENT_RENTAL_2 = aRentalExpiring(tomorrow());
-	private static final Rental EXPIRED_RENTAL = aRentalExpiring(yesterday());
+	private static final Rental EXPIRED_RENTAL_1 = aRentalExpiring(yesterday());
+	private static final Rental EXPIRED_RENTAL_2 = aRentalExpiring(yesterday());
 
 	private TransactionRepository transactionRepository = new SetBasedTransactionRepository();
 	private ViewCurrentRentalsAction action = new ViewCurrentRentalsAction(transactionRepository);
 
+	@Before
+	public void setUp() throws Exception {
+		when(request.getCustomer()).thenReturn(CUSTOMER);
+	}
+
 	@Test
-	public void passesOnlyCurrentRentalsToTheViewTemplate() throws Exception {
-		Transaction t1 = new Transaction(anyTime(), CUSTOMER, asSet(EXPIRED_RENTAL, CURRENT_RENTAL_1));
+	public void noCurrentRentalsByOurCustomer() throws Exception {
+		Transaction t1 = new Transaction(anyTime(), CUSTOMER, asSet(EXPIRED_RENTAL_1));
+		Transaction t2 = new Transaction(anyTime(), CUSTOMER, asSet(EXPIRED_RENTAL_2));
+		transactionRepository.add(asSet(t1, t2));
+
+		action.accept(request, response);
+
+		verify(response).putTemplateData("rentals", asSet());
+		verify(response).renderTemplate("rentals", "main_layout");
+	}
+
+	@Test
+	public void someRentalsCurrentSomeExpired() throws Exception {
+		Transaction t1 = new Transaction(anyTime(), CUSTOMER, asSet(EXPIRED_RENTAL_1, CURRENT_RENTAL_1));
 		Transaction t2 = new Transaction(anyTime(), CUSTOMER, asSet(CURRENT_RENTAL_2));
 		transactionRepository.add(asSet(t1, t2));
 
@@ -36,13 +58,20 @@ public class ViewCurrentRentalsActionTest extends BaseTestForVideoWorldApp {
 		verify(response).renderTemplate("rentals", "main_layout");
 	}
 
-	private LocalDateTime anyTime() {
-		return LocalDateTime.now();
+	@Test
+	public void ignoreRentalsByOtherCustomers() throws Exception {
+		Transaction transactionByOurCustomer = new Transaction(anyTime(), CUSTOMER, asSet(CURRENT_RENTAL_1));
+		Transaction transactionByAnotherCustomer =
+				new Transaction(anyTime(), ANOTHER_CUSTOMER,
+						asSet(new Rental(ANOTHER_CUSTOMER, anyMovie(), Period.of(yesterday(), tomorrow()))));
+		transactionRepository.add(asSet(transactionByOurCustomer, transactionByAnotherCustomer));
+
+		action.accept(request, response);
+
+		verify(response).putTemplateData("rentals", asSet(CURRENT_RENTAL_1));
+		verify(response).renderTemplate("rentals", "main_layout");
 	}
 
-	private static LocalDate yesterday() {
-		return LocalDate.daysBeforeToday(1);
-	}
 
 	private static Rental aRentalExpiring(LocalDate expiration) {
 		return new Rental(CUSTOMER, anyMovie(), Period.of(LocalDate.daysBeforeToday(100), expiration));
@@ -52,7 +81,15 @@ public class ViewCurrentRentalsActionTest extends BaseTestForVideoWorldApp {
 		return new Movie("some movie", Movie.REGULAR);
 	}
 
-	private static LocalDate tomorrow() {
+	private LocalDateTime anyTime() {
+		return LocalDateTime.now();
+	}
+
+	private static FiniteLocalDate yesterday() {
+		return LocalDate.daysBeforeToday(1);
+	}
+
+	private static FiniteLocalDate tomorrow() {
 		return LocalDate.daysAfterToday(1);
 	}
 
