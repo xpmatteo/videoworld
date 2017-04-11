9094858894ee53c842e8645f5ff3a28ba1ea674d
Luan Reis <luan.rpsantos@gmail.com>
2017-04-11 15:13:53 +0200

    Remove unnecessary import of WebResponse interface

--- src/main/java/com/thoughtworks/videorental/toolkit/web/ServletWebResponse.java
+++ src/main/java/com/thoughtworks/videorental/toolkit/web/ServletWebResponse.java
@@ -1,21 +1,18 @@
 package com.thoughtworks.videorental.toolkit.web;
 
+import com.thoughtworks.videorental.domain.Customer;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.thoughtworks.videorental.domain.Customer;
-import com.thoughtworks.videorental.toolkit.web.WebResponse;
-
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
 
 public class ServletWebResponse implements WebResponse {
 
